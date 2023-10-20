6f8703a99ee07b3b11a75998740d28b4f9da78b4
bomcrmon <110074089+bomcrmon@users.noreply.github.com>
2023-10-20 15:15:19 +0700

    f

--- app8-1/local/src/App.js
+++ app8-1/local/src/App.js
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 import DBNav from './db-nav'
 
 export default function App(){
   return <DBNav/>
 }
+=======
+import React from 'react'
+import FormGet from './form-get'
+
+export default function App() {
+  return <FormGet/>
+}
+>>>>>>> parent of 5f642d0 (add form-post)
--- app8-1/server/index.js
+++ app8-1/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+<<<<<<< HEAD
 const Product = require('./model')
 const port = 8000
 
@@ -115,4 +116,25 @@ app.get('/api/db/search', (request, response) => {
 
 app.listen(port, () => {
 	console.log('Server listening on port ' + port)
+=======
+const port = 8000
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/api/form-get', (request, response) => {
+    let t = request.query.target || ''
+    let k = request.query.kw || ''
+    let n = parseInt((Math.random() * 1000))
+    let r = {
+        target: t,
+        kw: k,
+        results: n
+    }
+    response.json(r)
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port ' + port)
+>>>>>>> parent of 5f642d0 (add form-post)
 })
